97e5fb66b1e12e3059d5c3ba4e6138971e50283e
Nicholas Nagle <nnagle@nnagle.nomad.utk.edu>
2015-02-05 12:54:06 -0500

    added comments to download_itrdb

--- R/01_download_itrdb.R
+++ R/01_download_itrdb.R
@@ -27,17 +27,27 @@ read.crn.head <- function(fname){
   else return(NULL)
 }
 
+# Get list of files in itrdb folder
 files <- list.files('~/Dropbox/git_root/climate-bayes/data/itrdb', full.names=TRUE)
+# Filter file names to be "standard" chronologies.  No exotics like "latewood", "arstan" etc.
 files <- files[grepl('[[:digit:]]\\.crn', files)]
+# Feed files through my read.crn.head
 temp <- lapply(files,read.crn.head)
+# rbind that into a data.frame
 temp <- do.call(rbind, temp)
+# filter that data.frame to have valid lat-lon strings and valid year ranges
 df <- temp %>% filter(grepl(lat_lon, pattern='[[:digit:]]{4,4}-[[:digit:]]{5,5}')) %>% filter(last<2015)
+# Create properly formatted lat/lon coordinates (convert from dddmm to decimal)
 df <- df %>% mutate(lat=as.numeric(substr(lat_lon, 1, 2))+as.numeric(substr(lat_lon, 3, 4))/60,
                     lon=as.numeric(substr(lat_lon, 6, 8))+as.numeric(substr(lat_lon, 9, 10))/60)
 
+# Plot the lat/lon
 df %>% ggplot() + geom_point(aes(x=-lon, y=lat))
-which(table(df$species_code)>50)
-df %>% filter( species_code %in% names(table(df$species_code))[table(df$species_code)>20] ) -> temp2
+which(table(df$species_code)>20)
+# Filter database on species with more than 20 series.
+df %>% filter( species_code %in% names(table(df$species_code))[table(df$species_code)>20] ) -> df.clean
+
+# Plot the lat/lon again.
 temp2 %>% ggplot() + geom_point(aes(x=-lon, y=lat, color=species_code))
 
 
